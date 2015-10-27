954159feca60b7b4e9c55933b513a5925c27c8e5
Chris Gathercole <Chris.Gathercole@ft.com>
2015-10-27 15:06:41 +0000

    MODIFIED: big refactor, tests fixed, maxTokens is now a zero-padded string

--- test.js
+++ test.js
@@ -2,24 +2,31 @@ import test from 'ava';
 import fn from './';
 
 const invalidShareCode = '';
-const invalidUserId = '';
+const invalidUserId    = '';
 const invalidArticleId = '';
-const invalidSalt = '';
+const invalidSalt      = '';
+const invalidTime      = 'abc';
+const invalidMaxTokens = 'abc';
 
-const validShareCode = 'kevj9c864ph74yza7d0pt9vfjzs765kk';
-const validUserId = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa';
-const validArticleId = 'bbbbbbbb-aaaa-bbbb-aaaa-bbbbbbbbbbbb';
-const validSalt = '12345678901234567890123456789012';
+const validUserId     = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa';
+const validUserId2    = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaabbb';
+const validArticleId  = 'bbbbbbbb-aaaa-bbbb-aaaa-bbbbbbbbbbbb';
+const validArticleId2 = 'bbbbbbbb-aaaa-bbbb-aaaa-bbbbbbbbbccc';
+const validSalt       = '1234567890123456789012345678901234567890123456';
+const validTime       = 1234567890;
+const validMaxTokens  = 100;
 
 test('encrypting should throw for invalid values', t => {
-	t.throws(fn.encrypt.bind(fn,invalidUserId, validArticleId, validSalt), Error);
-	t.throws(fn.encrypt.bind(fn,validUserId, invalidArticleId, validSalt), Error);
-	t.throws(fn.encrypt.bind(fn,validUserId, validArticleId, invalidSalt), Error);
+	t.throws(fn.encrypt.bind(fn,invalidUserId,   validArticleId,   validSalt,   validTime,   validMaxTokens), Error);
+	t.throws(fn.encrypt.bind(fn,  validUserId, invalidArticleId,   validSalt,   validTime,   validMaxTokens), Error);
+	t.throws(fn.encrypt.bind(fn,  validUserId,   validArticleId, invalidSalt,   validTime,   validMaxTokens), Error);
+	t.throws(fn.encrypt.bind(fn,  validUserId,   validArticleId,   validSalt, invalidTime,   validMaxTokens), Error);
+	t.throws(fn.encrypt.bind(fn,  validUserId,   validArticleId,   validSalt,   validTime, invalidMaxTokens), Error);
 	t.end();
 });
 
 test('encrypting should return a string', t => {
-	t.is(typeof fn.encrypt(validUserId, validArticleId, validSalt), 'string');
+	t.is(typeof fn.encrypt(validUserId, validArticleId, validSalt, validTime, validMaxTokens), 'string');
 	t.end();
 });
 
@@ -30,22 +37,25 @@ test('decrypting should throw for invalid values', t => {
 	t.end();
 });
 
-test('decrypting should return a string', t => {
-	t.is(typeof fn.decrypt(validShareCode, validArticleId, validSalt), 'string');
-	t.end();
-});
-
 test('decrypting should return the User ID when given an encrypted string', t => {
-	t.is(fn.decrypt(fn.encrypt(validUserId, validArticleId, validSalt), validArticleId, validSalt), validUserId);
+	var code = fn.encrypt(validUserId, validArticleId, validSalt, validTime, validMaxTokens);
+	t.is(fn.decrypt(code, validArticleId, validSalt)['user'], validUserId);
 	t.end()
 });
 
 test('should return true if code conforms to share code pattern', t => {
-	t.is(fn.isShareCodePattern(validShareCode), true);
+	var code = fn.encrypt(validUserId, validArticleId, validSalt, validTime, validMaxTokens);
+	t.is(fn.isShareCodePattern(code), true);
 	t.end();
 });
 
 test('should return false if code does not conform to share code pattern', t => {
 	t.is(fn.isShareCodePattern(invalidShareCode), false);
+	t.end();
+});
+
+test('decrypting with a different articleId should not return the original User ID', t => {
+	var code = fn.encrypt(validUserId, validArticleId, validSalt, validTime, validMaxTokens);
+	t.not(fn.decrypt(code, validArticleId2, validSalt)['user'], validUserId);
 	t.end();
 });
