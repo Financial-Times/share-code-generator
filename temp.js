677ba6c2a9126f0ebdf0ea0537354d3d4bca3420
Chris Gathercole <Chris.Gathercole@ft.com>
2015-11-18 11:05:56 +0000

    MODIFIED; generating pem via diffieh

--- temp.js
+++ temp.js
@@ -3,15 +3,24 @@
 // var NodeRSA = require('node-rsa');
 var crypto = require('crypto');
 
-var pem = '-----BEGIN RSA PRIVATE KEY-----\n'+
-          'MIIBOQIBAAJAVY6quuzCwyOWzymJ7C4zXjeV/232wt2ZgJZ1kHzjI73wnhQ3WQcL\n'+
-          'DFCSoi2lPUW8/zspk0qWvPdtp6Jg5Lu7hwIDAQABAkBEws9mQahZ6r1mq2zEm3D/\n'+
-          'VM9BpV//xtd6p/G+eRCYBT2qshGx42ucdgZCYJptFoW+HEx/jtzWe74yK6jGIkWJ\n'+
-          'AiEAoNAMsPqwWwTyjDZCo9iKvfIQvd3MWnmtFmjiHoPtjx0CIQCIMypAEEkZuQUi\n'+
-          'pMoreJrOlLJWdc0bfhzNAJjxsTv/8wIgQG0ZqI3GubBxu9rBOAM5EoA4VNjXVigJ\n'+
-          'QEEk1jTkp8ECIQCHhsoq90mWM/p9L5cQzLDWkTYoPI49Ji+Iemi2T5MRqwIgQl07\n'+
-          'Es+KCn25OKXR/FJ5fu6A6A+MptABL3r8SEjlpLc=\n'+
-          '-----END RSA PRIVATE KEY-----';
+var sha = 'sha512';
+var prime_length = 60;
+var diffieHellman = crypto.createDiffieHellman(prime_length);
+var genKeys = diffieHellman.generateKeys('hex');
+
+var pem = diffieHellman.getPrivateKey('hex');
+
+console.log("via DiffieHellman: prime_length=" + prime_length + ", pem=" + pem + "\n");
+
+// var pem = '-----BEGIN RSA PRIVATE KEY-----\n'+
+//           'MIIBOQIBAAJAVY6quuzCwyOWzymJ7C4zXjeV/232wt2ZgJZ1kHzjI73wnhQ3WQcL\n'+
+//           'DFCSoi2lPUW8/zspk0qWvPdtp6Jg5Lu7hwIDAQABAkBEws9mQahZ6r1mq2zEm3D/\n'+
+//           'VM9BpV//xtd6p/G+eRCYBT2qshGx42ucdgZCYJptFoW+HEx/jtzWe74yK6jGIkWJ\n'+
+//           'AiEAoNAMsPqwWwTyjDZCo9iKvfIQvd3MWnmtFmjiHoPtjx0CIQCIMypAEEkZuQUi\n'+
+//           'pMoreJrOlLJWdc0bfhzNAJjxsTv/8wIgQG0ZqI3GubBxu9rBOAM5EoA4VNjXVigJ\n'+
+//           'QEEk1jTkp8ECIQCHhsoq90mWM/p9L5cQzLDWkTYoPI49Ji+Iemi2T5MRqwIgQl07\n'+
+//           'Es+KCn25OKXR/FJ5fu6A6A+MptABL3r8SEjlpLc=\n'+
+//           '-----END RSA PRIVATE KEY-----';
 
 var text = 'ABCDEFghijklMNOPQRstuvWxYz12344466475654jlh64jkgh4hkghk54ghk5';
 
@@ -28,7 +37,6 @@ var text = 'ABCDEFghijklMNOPQRstuvWxYz12344466475654jlh64jkgh4hkghk54ghk5';
 // 	"checkSig=" + checkSig
 // 	].join("\n"));
 
-var sha = 'sha512';
 
 // var hash = crypto.createHash(sha);
 // hash.update( text );
